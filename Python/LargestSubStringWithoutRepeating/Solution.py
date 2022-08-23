10efb09dbde0bb321f52f4f6d0c42ca9265a0f51
Matheus Ibrahim <matheusibrahimfm@gmail.com>
2022-08-23 11:19:51 -0300

    Adds test cases and solves bug of emptying the dict

--- Python/LargestSubStringWithoutRepeating/Solution.py
+++ Python/LargestSubStringWithoutRepeating/Solution.py
@@ -1,21 +1,41 @@
-class Solution:
-  def lengthOfLongestSubstring(self, s):
-      n = len(s)
-      largestSubstring = 0
-      currentSubstring = 0
-      charsInSubstring = {}
-      for i in range(n):
-          c = s[i]
-          if c in charsInSubstring:
-            charsInSubstring = {key:value for key, value in charsInSubstring.items() if value > i}
-            currentSubstring = len(charsInSubstring)
+def lengthOfLongestSubstring(s):
+    n = len(s)
+    if n == 0:
+      return -1
+    
+    if n == 1:
+      return 1
+    
+    largestSubstring = 0
+    currentSubstring = 0
+    charsInSubstring = {}
+    
+    for i in range(n):
+        c = s[i]
+        if c in charsInSubstring:
+          charsInSubstring = {key:value for key, value in charsInSubstring.items() if value > charsInSubstring[c]}
+          currentSubstring = len(charsInSubstring)
+        
+        charsInSubstring[c] = i
+        currentSubstring += 1
+        if currentSubstring > largestSubstring:
+          largestSubstring = currentSubstring
           
-          charsInSubstring[c] = i
-          currentSubstring += 1
-          if currentSubstring > largestSubstring:
-            largestSubstring = currentSubstring
-      return largestSubstring
+    return largestSubstring
               
-
-print(Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx'))
-# 10
+print(lengthOfLongestSubstring('geeksforgeeks')) 
+#7
+print(lengthOfLongestSubstring('aabrakkaershaopgdysyyys')) 
+#10
+print(lengthOfLongestSubstring('abrkaabcdefghijjxxx')) 
+#10
+print(lengthOfLongestSubstring('')) 
+# #-1
+print(lengthOfLongestSubstring('a')) 
+# #1
+print(lengthOfLongestSubstring('aaaaaaaaaaaaaaaaaaaaaaaaaaa')) 
+# #1
+print(lengthOfLongestSubstring('abcdefghijklmnopqrstuvwxyz')) 
+# #26
+print(lengthOfLongestSubstring('aaaabaaaa')) 
+# #2
