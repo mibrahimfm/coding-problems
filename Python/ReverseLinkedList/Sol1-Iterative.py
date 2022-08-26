0c56e4c02450e55f8b3e96b30f4f699acd059a6a
Matheus Ibrahim <matheusibrahimfm@gmail.com>
2022-08-26 13:37:11 -0300

    Add iterative solution to ReverseLinkedList

---
+++ Python/ReverseLinkedList/Sol1-Iterative.py
@@ -0,0 +1,50 @@
+class ListNode(object):
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+  
+  # Function to print the list
+  def printList(self):
+    node = self
+    output = '' 
+    while node != None:
+      output += str(node.val)
+      output += " "
+      node = node.next
+    print(output)
+
+  # Iterative Solution
+  def reverse(self, head):
+    prev = None
+    next = None
+    
+    while head is not None:
+        next = head.next
+        head.next = prev
+        prev = head
+        head = next
+        
+    head = prev
+    
+
+
+# Test Program
+# Initialize the test list: 
+testHead = ListNode(4)
+node1 = ListNode(3)
+testHead.next = node1
+node2 = ListNode(2)
+node1.next = node2
+node3 = ListNode(1)
+node2.next = node3
+testTail = ListNode(0)
+node3.next = testTail
+
+print("Initial list: ")
+testHead.printList()
+# 4 3 2 1 0
+testHead.reverse(testHead)
+#testHead.reverseRecursively(testHead)
+print("List after reversal: ")
+testTail.printList()
+# 0 1 2 3 4
